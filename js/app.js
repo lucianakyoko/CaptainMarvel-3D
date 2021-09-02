cd04b34a8ff0115b82dc6d4d222573421f37ecc4
Luciana Kyoko <69259218+lucianakyoko@users.noreply.github.com>
2021-09-02 14:45:19 -0300

    Adding animations and parallax effects

---
+++ js/app.js
@@ -0,0 +1,18 @@
+gsap.to('[data-layer3]', {
+  scrollTrigger: {
+    scrub: true
+  },
+  x: 1000,
+  y: 200,
+  scale: 4
+});
+
+gsap.to('[data-layer4]', {
+  scrollTrigger: {
+    scrub: true
+  },
+  x: 0,
+  y: -1800,
+  scale: 2
+});
+
